4278db59bd62b30dd0574c34bcd6879661b1e9af
Aidin Poorsolhjouy <idn.poorsolhjouy@gmail.com>
2026-09-25 00:03:43 +0200

    hivemq connection test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,46 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import * as mqtt from 'mqtt';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('mqtt-test')
+  async testMqtt() {
+    return new Promise((resolve) => {
+      const client = mqtt.connect(process.env.HIVEMQ_URL!, {
+        username: process.env.HIVEMQ_USERNAME,
+        password: process.env.HIVEMQ_PASSWORD,
+        rejectUnauthorized: true,
+        connectTimeout: 10000,
+      });
+
+      const timer = setTimeout(() => {
+        client.end(true);
+        resolve({
+          success: false,
+          message: 'MQTT connection timeout',
+        });
+      }, 12000);
+
+      client.on('connect', () => {
+        clearTimeout(timer);
+        client.end(true);
+
+        resolve({
+          success: true,
+          message: 'Connected successfully to HiveMQ',
+        });
+      });
+
+      client.on('error', (error) => {
+        clearTimeout(timer);
+        client.end(true);
+
+        resolve({
+          success: false,
+          message: error.message,
+        });
+      });
+    });
   }
 }
